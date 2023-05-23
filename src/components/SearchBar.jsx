cc53491079ad16254964c7c622a9f5b704be857e
itsmefaishal <faishal.rahman.79@gmail.com>
2023-05-23 21:45:38 +0530

    passing data as prop

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -43,7 +43,7 @@ const SearchBar = () => {
       >
         Search
       </button>
-      {showResults && <SearchResults />}
+      {showResults && <SearchResults data={data} />}
     </div>
   );
 };
